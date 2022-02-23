943e792d08c2652efa607e2c74211bcc292a0bf5
Prince819794 <saikiapp9@gmail.com>
2022-02-23 13:50:41 +0530

    init commit

---
+++ pages/qr.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Qr from '../style-guide/page-component/Qr'
+
+const qr_page = () => {
+  return (
+    <div>
+    <Qr/>
+    </div>
+  )
+}
+
+export default qr_page
