9ad2aaa8e1c2b989e6b9c7ad93534ebebb744dd9
Prince819794 <saikiapp9@gmail.com>
2022-02-25 12:43:14 +0530

    loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
-
+import Load from "../style-guide/components/lottie/loader";
+import React,{useState, useEffect} from "react";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() =>{
+    // setTimeout(() => setLoading(false), 5000);
+    setLoading(false),[]
+   })
+
+  return (
+    <>
+      {!loading ? (
+        <React.Fragment>
+          <Component {...pageProps} />
+        </React.Fragment>
+      ) : (
+        <Load/>
+      )}
+    </>
+  );
 }
 
 export default MyApp
